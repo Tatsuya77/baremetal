4bf2c5b4483e6de68f413a8e6f97e73413181c3c
Tatsuya77 <tatsuyakubo4869@gmail.com>
2020-08-13 08:58:23 +0900

    10-C: fix

--- kernel/port_io.s
+++ kernel/port_io.s
@@ -9,21 +9,21 @@ load_tmr:
 .global write_conf_address
 write_conf_address:
   mov %edi, %eax 
-  mov $0xcf8, %edx
-  out %eax, %edx
+  mov $0xcf8, %dx
+  out %eax, %dx
   ret
 
 # uint32_t read_conf_data(void);
 .global read_conf_data
 read_conf_data:
-  mov $0xcfc, %edx
-  in %edx, %eax
+  mov $0xcfc, %dx
+  in %dx, %eax
   ret
 
 # void write_conf_data(unsigned int data);
 .global write_conf_data
 write_conf_data:
   mov %edi, %eax
-  mov $0xcfc, %edx
-  out %eax, %edx
+  mov $0xcfc, %dx
+  out %eax, %dx
   ret
