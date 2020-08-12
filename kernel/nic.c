3a0c183f75f11763027b481243573a23cb03e40f
Tatsuya77 <tatsuyakubo4869@gmail.com>
2020-08-13 08:10:43 +0900

    10-B: fixed

--- kernel/nic.c
+++ kernel/nic.c
@@ -16,10 +16,10 @@ static unsigned int nic_base_address;
 
 static struct TxDescriptor tx_descriptors[TX_DESCRIPTORS_NUM]__attribute__((aligned(16)));
 
-static unsigned int tx_current_idx;
+// static unsigned int tx_current_idx;
 
 static void set_nic_register(unsigned short offset, unsigned int value) {
-    *(unsigned int *)(nic_base_address + offset) = value;
+    *(unsigned int *)(unsigned long long)(nic_base_address + offset) = value;
 }
 
 static void init_tx() {
@@ -36,7 +36,7 @@ static void init_tx() {
     /* TDH */
     set_nic_register(0x3810, (unsigned int) 0);
     /* TDT */
-    set_nic_register(0x3818, (unsigned int) 1);
+    set_nic_register(0x3818, (unsigned int) 0);
     /* TCTL */
     set_nic_register(0x400, (unsigned int) 0b1010 << 22 | 0x40 << 12 | 0x0f << 4 | 0b1010);
 }
@@ -45,18 +45,18 @@ void init_nic(unsigned int nic_address) {
     nic_base_address = nic_address;
 
     /* IMC */
-    set_nic_register(0x00d8, 0xffffffff);
+    set_nic_register(0x00d8, 0x0000ffff);
 
     init_tx();
 }
 
 unsigned char send_frame(void *buffer, unsigned short len) {
-    for (unsigned int i=0; i<len; i++) {
+    for (unsigned int i=0; i<1; i++) {
         tx_descriptors[(0+i)%TX_DESCRIPTORS_NUM].buf_addr = (unsigned long long) buffer + i;
         /* length */
-        tx_descriptors[(0+i)%TX_DESCRIPTORS_NUM].legacy = tx_descriptors[(0+i)%TX_DESCRIPTORS_NUM].legacy | 0x8;
+        tx_descriptors[(0+i)%TX_DESCRIPTORS_NUM].legacy = tx_descriptors[(1+i)%TX_DESCRIPTORS_NUM].legacy | len;
         /* TDT */
-        set_nic_register(0x3818, (unsigned int) (2+i)%TX_DESCRIPTORS_NUM);
+        set_nic_register(0x3818, (unsigned int) (1+i)%TX_DESCRIPTORS_NUM);
 
         while (((tx_descriptors[(0+i)%TX_DESCRIPTORS_NUM].legacy >> 32) & 0b1) == 0);
     }
